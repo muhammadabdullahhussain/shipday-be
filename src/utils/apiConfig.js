1ac5d143d0cb3a1d105f5347744cf503403c6c71
prabakaransquash <Prabhakaran@squashapps.com>
2025-11-04 11:08:02 +0530

    apiconfig

--- src/utils/apiConfig.js
+++ src/utils/apiConfig.js
@@ -1,15 +1,15 @@
 // src/utils/apiConfig.js
 
-// Backend base API
-// const BASE_URL = "https://swiftship-be-bxcwgcbzauhuekas.canadacentral-01.azurewebsites.net/api";
-
-// // Socket.IO server (no trailing slash)
-// const SOCKET_IO_URL = "https://swiftship-be-bxcwgcbzauhuekas.canadacentral-01.azurewebsites.net";
- 
-const BASE_URL = "/api";
+//Backend base API
+const BASE_URL = "https://swiftship-be-bxcwgcbzauhuekas.canadacentral-01.azurewebsites.net/api";
 
 // Socket.IO server (no trailing slash)
-const SOCKET_IO_URL = "http://localhost:5000";
+const SOCKET_IO_URL = "https://swiftship-be-bxcwgcbzauhuekas.canadacentral-01.azurewebsites.net";
+ 
+// const BASE_URL = "/api";
+
+// // Socket.IO server (no trailing slash)
+// const SOCKET_IO_URL = "http://localhost:5000";
 
 export default BASE_URL;
 export { BASE_URL, SOCKET_IO_URL };
